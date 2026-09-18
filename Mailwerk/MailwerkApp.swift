a6a2cbc6f1e2738c7ddb1f66812bf56fbb80c461
Kim Sieber <kim@ordinum.com>
2026-09-18 21:14:48 +0000

    Add Xcode project structure

---
+++ Mailwerk/MailwerkApp.swift
@@ -0,0 +1,17 @@
+//
+//  MailwerkApp.swift
+//  Mailwerk
+//
+//  Created by Kim Sieber on 18.09.26.
+//
+
+import SwiftUI
+
+@main
+struct MailwerkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MailwerkTests/MailwerkTests.swift
@@ -0,0 +1,18 @@
+//
+//  MailwerkTests.swift
+//  MailwerkTests
+//
+//  Created by Kim Sieber on 18.09.26.
+//
+
+import Testing
+
+struct MailwerkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
